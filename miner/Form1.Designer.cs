4af4dbeb2066534706fcae63e4bf9626a2407c32
Robert Call <robertcall94@gmail.com>
2019-03-12 23:21:06 +0300

    Update

---
+++ miner/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace miner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.square = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Change_button2 = new System.Windows.Forms.Button();
+            this.Change_button1 = new System.Windows.Forms.Button();
+            this.Numeric_Row = new System.Windows.Forms.NumericUpDown();
+            this.Numeric_Col = new System.Windows.Forms.NumericUpDown();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Row)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Col)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // square
+            // 
+            this.square.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.square.AutoScroll = true;
+            this.square.BackColor = System.Drawing.SystemColors.Control;
+            this.square.Location = new System.Drawing.Point(12, 103);
+            this.square.Name = "square";
+            this.square.Size = new System.Drawing.Size(400, 350);
+            this.square.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.Change_button2);
+            this.groupBox1.Controls.Add(this.Change_button1);
+            this.groupBox1.Controls.Add(this.Numeric_Row);
+            this.groupBox1.Controls.Add(this.Numeric_Col);
+            this.groupBox1.Controls.Add(this.ResetButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 68);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Change_button2
+            // 
+            this.Change_button2.Enabled = false;
+            this.Change_button2.Location = new System.Drawing.Point(365, 39);
+            this.Change_button2.Name = "Change_button2";
+            this.Change_button2.Size = new System.Drawing.Size(25, 25);
+            this.Change_button2.TabIndex = 19;
+            this.Change_button2.Text = "b";
+            this.Change_button2.UseVisualStyleBackColor = true;
+            this.Change_button2.Click += new System.EventHandler(this.Change_button2_Click);
+            // 
+            // Change_button1
+            // 
+            this.Change_button1.Location = new System.Drawing.Point(334, 39);
+            this.Change_button1.Name = "Change_button1";
+            this.Change_button1.Size = new System.Drawing.Size(25, 25);
+            this.Change_button1.TabIndex = 18;
+            this.Change_button1.Text = "f";
+            this.Change_button1.UseVisualStyleBackColor = true;
+            this.Change_button1.Click += new System.EventHandler(this.Change_button1_Click);
+            // 
+            // Numeric_Row
+            // 
+            this.Numeric_Row.Location = new System.Drawing.Point(6, 19);
+            this.Numeric_Row.Name = "Numeric_Row";
+            this.Numeric_Row.Size = new System.Drawing.Size(75, 20);
+            this.Numeric_Row.TabIndex = 17;
+            this.Numeric_Row.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // Numeric_Col
+            // 
+            this.Numeric_Col.Location = new System.Drawing.Point(87, 19);
+            this.Numeric_Col.Name = "Numeric_Col";
+            this.Numeric_Col.Size = new System.Drawing.Size(75, 20);
+            this.Numeric_Col.TabIndex = 16;
+            this.Numeric_Col.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(168, 19);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 20);
+            this.ResetButton.TabIndex = 15;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.Reset);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 464);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.square);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Row)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric_Col)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel square;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown Numeric_Row;
+        private System.Windows.Forms.NumericUpDown Numeric_Col;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button Change_button2;
+        private System.Windows.Forms.Button Change_button1;
+    }
+}
+
---
+++ miner/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+        int row, col;
+        char[,] bomb_square;
+        Button[,] But;
+        bool flag = false;
+        int num_flag = 0;
+        int right_flag = 0;
+
+        private void Square_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Bomb_Click(b.Location.Y / 25, b.Location.X / 25);
+        }
+
+        private void Bomb_Click(int i, int j)
+        {
+            if (0 <= i && i <= 9 && 0 <= j && j <= 9 && But[i,j].Enabled)
+            {
+                if (flag == false)
+                {
+                    if (But[i, j].Text != "f")
+                    {
+                        But[i, j].Text = bomb_square[i + 1, j + 1].ToString();
+                        But[i, j].Enabled = false;
+                        if (bomb_square[i + 1, j + 1] == '0')
+                        {
+                            Bomb_Click(i - 1, j - 1);
+                            Bomb_Click(i - 1, j);
+                            Bomb_Click(i - 1, j + 1);
+                            Bomb_Click(i, j - 1);
+                            Bomb_Click(i, j + 1);
+                            Bomb_Click(i + 1, j - 1);
+                            Bomb_Click(i + 1, j);
+                            Bomb_Click(i + 1, j + 1);
+                        }
+                        if (bomb_square[i + 1, j + 1] == 'B')
+                        {
+                            fail(row, col);
+                        }
+                    }
+                }
+                else
+                {
+                    if(But[i,j].Text!="f")
+                    {
+                        But[i, j].Text = "f";
+                        num_flag++;
+                        if (bomb_square[i + 1, j + 1] == 'B') right_flag++;
+                    }
+                    else
+                    {
+                        But[i, j].Text = "";
+                        num_flag--;
+                        if (bomb_square[i + 1, j + 1] == 'B') right_flag--;
+                    }
+                    if (right_flag == num_flag && num_flag == row * col / 4) MessageBox.Show("You win!!");
+                }
+            }
+        }
+
+        private void fail(int r,int c)
+        {
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < c; j++)
+                {
+                    But[i, j].Text = bomb_square[i + 1, j + 1].ToString();
+                    But[i, j].Enabled = false;
+                }
+            MessageBox.Show("Fail");
+        }
+
+        private char[,] get_square(int r, int c)
+        {
+            char[,] b = new char[r + 2, c + 2];
+            for (int i = 0; i < row * col / 4; i++)
+            {
+                int loc_row = rand.Next(row) + 1, loc_col = rand.Next(col) + 1;
+                while (b[loc_row, loc_col] == 'B')
+                {
+                    loc_row = rand.Next(row) + 1;
+                    loc_col = rand.Next(col) + 1;
+                }
+                b[loc_row, loc_col] = 'B';
+            }
+
+            for (int i = 1; i < row + 1; i++)
+                for (int j = 1; j < col + 1; j++)
+                {
+                    if (b[i, j] == 'B') continue;
+                    int bomb = 0;
+                    if (b[i - 1, j - 1] == 'B') bomb++;
+                    if (b[i - 1, j] == 'B') bomb++;
+                    if (b[i - 1, j + 1] == 'B') bomb++;
+                    if (b[i, j - 1] == 'B') bomb++;
+                    if (b[i, j + 1] == 'B') bomb++;
+                    if (b[i + 1, j - 1] == 'B') bomb++;
+                    if (b[i + 1, j] == 'B') bomb++;
+                    if (b[i + 1, j + 1] == 'B') bomb++;
+                    b[i, j] = (char)('0' + bomb);
+                }
+            return b;
+        }
+
+        private void Change_button1_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            Change_button1.Enabled = false;
+            Change_button2.Enabled = true;
+        }
+
+        private void Change_button2_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            Change_button1.Enabled = true;
+            Change_button2.Enabled = false;
+        }
+
+        private void Reset(object sender, EventArgs e)
+        {
+            square.Controls.Clear();
+
+            row = (int)Numeric_Row.Value;
+            col = (int)Numeric_Col.Value;
+
+            if (row > square.Height / 25) this.Height += (row - square.Height / 25) * 25;
+            if (col > square.Width / 25) this.Width += (col - square.Width / 25) * 25;
+
+            bomb_square = get_square(row,col);
+            
+            But = new Button[row, col];
+            
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    But[i, j] = new Button();
+                    But[i, j].Width = But[i, j].Height = 25;
+                    //But[i, j].Text = bomb_square[i+1,j+1].ToString();
+                    But[i, j].Location = new Point(j * 25, i * 25);
+                    But[i, j].Click += Square_Click;
+                    square.Controls.Add(But[i, j]);
+                }
+            }
+        }
+    }
+}
